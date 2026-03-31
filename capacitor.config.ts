c2dd46404d9d162d3bfac4d538bbe301b94d6558
Syeda-Data -Analytics <syedah2910@gmail.com>
2026-03-31 13:55:18 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.DermaScan.AI',
+  appName: 'DermaScanAI',
+  webDir: 'dist'
+};
+
+export default config;
